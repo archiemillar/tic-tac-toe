ea67f7998611a46abda91fd98e48d0e52e87030e
Archival Ray Millar <millar.archie@gmail.com>
2022-12-24 17:41:11 +0800

    improve computer player

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from player import Player
 T = "Tic Tac Toe"
 available_positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 gn = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+computer_last_move = 0
 
 
 # display grid
@@ -24,6 +25,63 @@ def current_player():
         return player_x
     else:
         return player_o
+
+
+def is_winner(pos):
+    return pos[0] == pos[1] == pos[2] or \
+           pos[3] == pos[4] == pos[5] or \
+           pos[6] == pos[7] == pos[8] or \
+           pos[0] == pos[3] == pos[6] or \
+           pos[1] == pos[4] == pos[7] or \
+           pos[2] == pos[5] == pos[8] or \
+           pos[0] == pos[4] == pos[8] or \
+           pos[2] == pos[4] == pos[6]
+
+
+def computer_move():
+    def is_smart(test_move, marking):
+        for n in available_positions:
+            smart_gn = gn.copy()
+            smart_gn[test_move - 1] = marking
+            smart_gn[n - 1] = marking
+            if is_winner(smart_gn):
+                return True
+
+    for mark in [player_x.mark, player_o.mark]:
+        for move in available_positions:
+            virtual_gn = gn.copy()
+            virtual_gn[move - 1] = mark
+            if is_winner(virtual_gn):
+                return move
+
+    if computer_last_move % 2 != 0 and 5 in available_positions:
+        random_num = random.randint(1, 2)
+        if random_num == 1:
+            if is_smart(5, current_player().mark):
+                return 5
+
+    if computer_last_move == 5:
+        for pos in available_positions:
+            if is_smart(pos, current_player().mark):
+                return pos
+
+    if computer_last_move in [1, 2, 3, 4, 6] and computer_last_move + 3 in available_positions:
+        if is_smart(computer_last_move + 3, current_player().mark):
+            return computer_last_move + 3
+
+    if computer_last_move in [1, 4, 7, 2, 8] and computer_last_move + 1 in available_positions:
+        if is_smart(computer_last_move + 1, current_player().mark):
+            return computer_last_move + 1
+
+    if computer_last_move in [4, 6, 7, 8, 9] and computer_last_move - 3 in available_positions:
+        if is_smart(computer_last_move - 3, current_player().mark):
+            return computer_last_move - 3
+
+    if computer_last_move in [2, 8, 3, 6, 9] and computer_last_move - 1 in available_positions:
+        if is_smart(computer_last_move - 1, current_player().mark):
+            return computer_last_move - 1
+
+    return random.choice(available_positions)
 
 
 # Create Players
@@ -63,7 +121,8 @@ if game_type == "single player":
 # Gameplay - Computer chooses a random number from available positions. Human player choose own number.
 while True:
     if current_player().is_computer:
-        number = random.choice(available_positions)
+        number = computer_move()
+        computer_last_move = number
     else:
         print("Choose a grid number.")
         show_grid()
@@ -83,9 +142,7 @@ while True:
         show_grid()
 
         # check if the player won or it's a draw
-        if (gn[0] == gn[1] == gn[2] or gn[3] == gn[4] == gn[5] or gn[6] == gn[7] == gn[8] or
-                gn[0] == gn[3] == gn[6] or gn[1] == gn[4] == gn[7] or gn[2] == gn[5] == gn[8] or
-                gn[0] == gn[4] == gn[8] or gn[2] == gn[4] == gn[6]):
+        if is_winner(gn):
             print(f"{current_player().name} Wins!")
             break
         elif not available_positions:
